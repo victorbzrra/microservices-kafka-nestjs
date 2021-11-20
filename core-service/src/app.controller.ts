f0186a295e38cf8d1cefadaa0e7b56ae6b543921
Victor Bezerra <victorbzrra@alu.ufc.br>
2021-11-20 00:30:08 -0300

    Teste de comunicação via kafka: Ok.

--- core-service/src/app.controller.ts
+++ core-service/src/app.controller.ts
@@ -1,9 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @MessagePattern('info-routine')
+  async index(@Payload() data: any): Promise<any> {
+    console.log(data.value)
+  }
 
   @Get()
   getHello(): string {
--- core-service/src/main.ts
+++ core-service/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'app',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'app-consumer',
+        allowAutoTopicCreation: true
+      }
+    }
+  });
+  await app.listen();
 }
 bootstrap();
