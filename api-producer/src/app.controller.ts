78c33ed0190476b1714efadef6807b510aabe72f
Victor Bezerra <victorbzrra@alu.ufc.br>
2021-11-19 22:51:08 -0300

    Criando api-producer.

--- api-producer/src/app.controller.ts
+++ api-producer/src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Observable } from 'rxjs';
+import { Body, Controller, OnModuleInit, Post } from '@nestjs/common';
+import { Client, ClientKafka, Transport,  } from '@nestjs/microservices';
+import { InfoRoutine } from './interfaces/info-interface';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+export class AppController implements OnModuleInit {
+  @Client({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'api',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'api-consumer',
+        allowAutoTopicCreation: true
+      }
+    }
+  })
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  private client: ClientKafka;
+  
+  async onModuleInit() {
+    const requestPatters = [
+      'info-routine'
+    ];
+
+    requestPatters.forEach(async pattern => {
+      this.client.subscribeToResponseOf(pattern);
+      await this.client.connect();
+    });
+  }
+
+  @Post()
+  infoRoutine(@Body() info: InfoRoutine ): Observable<InfoRoutine> {
+    return this.client.send('info-routine', info);
   }
 }
---
+++ api-producer/src/interfaces/info-interface.ts
@@ -0,0 +1,5 @@
+export interface InfoRoutine {
+  latitude: number,
+  longitude: number,
+  message: string
+}
